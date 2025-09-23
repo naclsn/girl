79c9b03b9abdbb62d6bb20c7a1ead750f38a288a
celestin-Thin-15-B12UCX <celestin.grenier@mhcomm.fr>
2025-09-23 15:11:05 +0200

    sqlite store gc old

--- girl/store/sqlite.py
+++ girl/store/sqlite.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from datetime import timedelta
 from pathlib import Path
 from sqlite3 import Connection
 
@@ -10,7 +12,17 @@ from .base import LoadedRun
 class BackendSqlite(Base):
     """ """
 
-    def __init__(self, path_or_conn: str | Path | Connection | aiosqlite.Connection):
+    def __init__(
+        self,
+        path_or_conn: str | Path | Connection | aiosqlite.Connection,
+        /,
+        # only keep so many entries
+        roll_nb_entries: int | None = None,
+        # remove (lazily) entries older than this
+        roll_old_entries: timedelta | None = None,
+        # keep (reported) size below this
+        # roll_total_bytes: float | None = None,
+    ):
         if isinstance(path_or_conn, Connection):
             self._path = None
             self._conn = aiosqlite.Connection(lambda: path_or_conn, 64)
@@ -19,6 +31,47 @@ class BackendSqlite(Base):
             self._conn = path_or_conn
         else:
             self._path = Path(path_or_conn)
+        self.roll_nb_entries = roll_nb_entries
+        self.roll_old_entries = roll_old_entries
+        # self.roll_total_bytes = roll_total_bytes
+
+    async def _roll_vacuum(self):
+        # we'll be deleting all entries before this ts (so by default none);
+        # the roll_.. by below do a `max(delts, ..)` meaning that whichever
+        # option makes it delete most prevail
+        delts = 0.0
+
+        if self.roll_nb_entries:
+            # if asked to roll on nb of entries, find the one that would leave
+            # this many after (sort by ts desc, offset skip nb, limit take 1)
+            c = await self._conn.execute(
+                rf"""
+ SELECT ts FROM event_runs ORDER BY ts DESC
+ LIMIT 1 OFFSET {self.roll_nb_entries}
+ """,
+                (),
+            )
+            # will not find any if there are less than nb entries total
+            if ts := await c.fetchone():
+                delts = max(delts, float(ts[0]))
+
+        if self.roll_old_entries:
+            delts = max(delts, (datetime.now() - self.roll_old_entries).timestamp())
+
+        # if self.roll_total_bytes:
+        #   an estimate can be done with a complex select-sum(length(data))-join
+        #   but for now nei idc
+
+        if delts:
+            await self._conn.executescript(
+                r"""
+ BEGIN;
+ DELETE FROM event_runs WHERE ts <= ?;
+ DELETE FROM run_data WHERE ts <= ?;
+ COMMIT;
+ """,  # VACUUM; -- don't vacuum, that would be too often
+                (delts, delts),
+            )
 
     async def storerun(self, id: str, runid: str, run: LoadedRun):
         """"""
@@ -32,6 +85,7 @@ class BackendSqlite(Base):
             [(runid, key, ts, data) for key, (ts, data) in data.items()],
         )
         await self._conn.commit()
+        await self._roll_vacuum()
 
     async def loadrun(self, id: str, runid: str):
         """"""
@@ -65,12 +119,13 @@ class BackendSqlite(Base):
  """,
             (),
         )
-        return f"{int((await c.fetchone())[0]):_} B"
+        return f"{int((await c.fetchone() or (0,))[0]):_} B"
 
     async def __aenter__(self):
         self._conn = await (aiosqlite.connect(self._path) if self._path else self._conn)
         await self._conn.executescript(
             r"""
+ BEGIN;
  CREATE TABLE IF NOT EXISTS event_runs (
     id    TEXT             NOT NULL, -- eg. "localhost:8080 GET /hi"
     runid TEXT PRIMARY KEY NOT NULL, -- eg. "some-banana"
@@ -84,6 +139,7 @@ class BackendSqlite(Base):
     FOREIGN KEY(runid) REFERENCES event_runs(runid),
     PRIMARY KEY(runid, key))
  STRICT, WITHOUT ROWID;
+ COMMIT;
  """,
         )
 
